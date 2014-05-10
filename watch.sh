3e5f3106ee22f30b4bac4756a9257ec7d69a3476
dhilipsiva <dhilipsiva@gmail.com>
2014-05-10 20:07:40 +0530

    Add a watch script

---
+++ watch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# watch.sh
+# Copyright (C) 2014 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+coffee -o src/js/ -bcw src/coffee/*.coffee
